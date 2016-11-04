452137058ff6e1242c37695015aa8350bee9db3b
Alejandro Solanas <asolanas93@gmail.com>
2016-11-04 15:40:04 +0100

    some examples with kivy gui

--- kivy/hello.py
+++ kivy/hello.py
@@ -4,11 +4,10 @@ kivy.require('1.9.1')
 from kivy.app import App
 from kivy.uix.label import Label
 
-class MyApp (App):
+class MyApp(App):
 
-    def build (self):
-        return Label(text='hello world')
-
+    def build(self):
+        return Label(text='Hello world')
 
 if __name__ == '__main__':
-    MyApp.run()
+    MyApp().run()
