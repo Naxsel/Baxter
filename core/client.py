72b90f62857ba990f75cb646aa66caeedcc30de1
Alejandro Solanas <asolanas93@gmail.com>
2016-11-08 13:48:50 +0100

    sample programm added

--- core/client.py
+++ core/client.py
@@ -11,19 +11,23 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
-
-def connect(instance):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # Create a socket object
-    ip = 'localhost'         # Get local machine name
-    port = 8080                 # Reserve a port for your service.
-
-    s.connect((ip, port))
-    print s.recv(1024)
-    print "Conected to Baxter!"
-    s.close                     # Close the socket when done
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+ip = 'localhost'  # Get local machine name
+port = 8080  # Reserve a port for your service.
 
 def bttn1(instance):
     print('The button <%s> is being pressed' % instance.text)
+
+def connect(instance):
+    s.connect((ip, port))
+    print s.recv(1024)
+    print "Conected to Baxter!"
+
+def toneutral(instance):
+    s.send("1")
+
+def disconnect(instance):
+    s.close                     # Close the socket when done
 
 class SuperButton(GridLayout):
 
@@ -37,6 +41,14 @@ class SuperButton(GridLayout):
         btn2= Button(text="Conexion")
         btn2.bind(on_press=connect)
         self.button = btn2
+        self.add_widget(self.button)
+        btn3= Button(text="Move to Neutral")
+        btn3.bind(on_press=toneutral)
+        self.button = btn3
+        self.add_widget(self.button)
+        btn4= Button(text="Disconnect")
+        btn4.bind(on_press=disconnect)
+        self.button = btn4
         self.add_widget(self.button)
 
 class MyApp(App):
--- core/server.py
+++ core/server.py
@@ -5,17 +5,27 @@ Main Server
 """
 
 import socket               # Import socket module
+import move_to_neutral
+import hello
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
-ip = 'localhost'  # Get local machine name
-print ip
-port = 8080                 # Reserve a port for your service.
-serversocket.bind((ip, port))        # Bind to the port
+def case(x):
+    {
+        '1': move_to_neutral.neutral(),
+        '2': hello.sayHello(),
+    }.get(x, 9)
 
-serversocket.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = serversocket.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   # script
-   c.send('Thank you for connecting')
-   c.close()                # Close the connection
+
+if __name__ == '__main__':
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+    ip = 'localhost'  # Get local machine name
+    port = 8080                 # Reserve a port for your service.
+    serversocket.bind((ip, port))        # Bind to the port
+    serversocket.listen(5)                 # Now wait for client connection.
+    print ("Socket listening at port <%s>" % port)
+
+    while True:
+       c, addr = serversocket.accept()     # Establish connection with client.
+       print 'Got connection from', addr
+
+       c.send('Thank you for connecting')
+       c.close()                # Close the connection
